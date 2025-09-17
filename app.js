d3dd6bfc94b525106fe166082f5e8d9b2d109a5e
ElYankee1221 <jorgesainzgil1@gmail.com>
2025-09-16 21:13:26 -0600

    implementacion de captura y validacion de nombres

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let amigos = [];
+
+function agregarAmigo() {
+    var amigo = document.getElementById("amigo").value;
+    //console.log(amigo);
+
+    if (amigo == "") {
+        alert("Por favor, inserte un nombre.");
+    } else {
+        console.log(amigo);
+        amigos.push(amigo);
+        console.log(amigos);
+        //document.getElementById("amigo").value = '';
+        document.querySelector("#amigo").value = '';
+    }
+
+}
+
+function sortearAmigo() {
+    
+}
